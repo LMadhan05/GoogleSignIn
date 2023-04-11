0747bb1999dc1cd26c9037f95eefe9faf035cc40
MadhanL05 <studymadhanl@gmail.com>
2023-04-12 00:55:53 +0530

    Email/Password Over

--- app/src/main/java/com/example/googlesignin/MainActivity.java
+++ app/src/main/java/com/example/googlesignin/MainActivity.java
@@ -1,14 +1,87 @@
 package com.example.googlesignin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+    Button login;
+    TextInputEditText email , password;
+
+    ProgressBar progress;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent obj = new Intent(getApplicationContext() , MainFrame.class);
+            startActivity(obj);
+            finish();
+        }
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progress = findViewById(R.id.progress);
+
+        login = findViewById(R.id.login);
+        email = findViewById(R.id.user_name);
+        password = findViewById(R.id.password);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progress.setVisibility(View.VISIBLE);
+
+                String email_ , password_ ;
+                email_ = String.valueOf(email.getText());
+                password_ = String.valueOf(password.getText());
+
+
+                mAuth.signInWithEmailAndPassword(email_, password_)
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progress.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(MainActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
+                                    Intent obj = new Intent(getApplicationContext() , MainActivity.class);
+                                    startActivity(obj);
+                                    finish();
+                                } else {
+                                    progress.setVisibility(View.GONE);
+                                    Toast.makeText(MainActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+
+            }
+        });
     }
 }
